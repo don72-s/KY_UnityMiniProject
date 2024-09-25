5f84699d3a3bf637340dc3502724b5c5ec362b19
김민태 <66211881+don72-s@users.noreply.github.com>
2024-09-25 12:32:47 +0900

    [feat] : 캐릭터 이동
    단일 입력에 따르는 캐릭터의 좌우 이동 구현, 이동은 자유이동이 아닌, 고정 라인의 전환 방식.

---
+++ Assets/Scripts/Character/Character.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    int moveDir = 1;
+    const int MAX_LANE = 3;
+    int curLane;
+
+    int centerOffset;
+
+    float destPosX;
+
+    [SerializeField]
+    float laneDistance;
+    [SerializeField]
+    float laneChangeSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        curLane = (MAX_LANE + 1) / 2;
+        centerOffset = curLane;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+
+            curLane += moveDir;
+
+            destPosX = (curLane - centerOffset) * laneDistance;
+
+            if (curLane == 1 || curLane == MAX_LANE) {
+                moveDir *= -1;
+            }
+
+        }
+
+        transform.position = Vector3.Lerp(transform.position,
+                                          new Vector3(destPosX, transform.position.y, transform.position.z),
+                                          laneChangeSpeed * Time.deltaTime);
+
+    }
+}
